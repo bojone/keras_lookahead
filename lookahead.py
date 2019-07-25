5aad03087dc4d614c93bef2ee17b16f0f1d36aa5
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-07-25 18:31:32 +0800

    Create lookahead.py

---
+++ lookahead.py
@@ -0,0 +1,53 @@
+from keras import backend as K
+
+
+class Lookahead:
+    """为Keras的优化器加入Lookahead功能
+    来源论文：https://arxiv.org/abs/1907.08610
+    """
+    def __init__(self, k=5, alpha=0.5):
+        self.k = k
+        self.alpha = alpha
+        self.count = 0
+    def inject(self, model):
+        """传入模型做注入
+        """
+        if not hasattr(model, 'train_function'):
+            raise RuntimeError('You must compile your model before using it.')
+        model._check_trainable_weights_consistency()
+        if model.train_function is None:
+            inputs = (model._feed_inputs +
+                      model._feed_targets +
+                      model._feed_sample_weights)
+            if model._uses_dynamic_learning_phase():
+                inputs += [K.learning_phase()]
+            fast_params = model._collected_trainable_weights
+            with K.name_scope('training'):
+                with K.name_scope(model.optimizer.__class__.__name__):
+                    training_updates = model.optimizer.get_updates(
+                        params=fast_params,
+                        loss=model.total_loss)
+                    slow_params = [K.zeros_like(p) for p in fast_params]  
+                fast_updates = (model.updates +
+                                training_updates +
+                                model.metrics_updates)
+                slow_updates, copy_updates = []
+                for p, q in zip(fast_params, slow_params):
+                    slow_updates.append(K.update(q, q + self.alpha * (p - q)))
+                    copy_updates.append(K.update(p, q))
+                # Gets loss and metrics. Updates weights at each call.
+                fast_train_function = K.function(
+                    inputs,
+                    [model.total_loss] + model.metrics_tensors,
+                    updates=fast_updates,
+                    name='fast_train_function',
+                    **model._function_kwargs)
+                def F(inputs):
+                    self.count += 1
+                    if self.count % self.k == 1:
+                        K.batch_get_value(copy_updates)
+                    R = fast_train_function(inputs)
+                    if self.count % self.k == 0:
+                        K.batch_get_value(slow_updates)
+                    return R
+                model.train_function = F
